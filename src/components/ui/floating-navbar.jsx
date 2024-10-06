ce0cb3578aef5d03c7760a2a68dbd42e11c360ff
Aditya <101622021+Kheraditya@users.noreply.github.com>
2024-10-06 23:08:42 +0530

    new header

--- src/components/ui/floating-navbar.jsx
+++ src/components/ui/floating-navbar.jsx
@@ -8,11 +8,10 @@ import {
 } from "framer-motion";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { IconHome, IconMessage } from "@tabler/icons-react";
+import Image from "next/image";
 
-export const FloatingNav = ({
-  navItems,
-  className
-}) => {
+export const FloatingNav = ({ navItems, className }) => {
   const { scrollYProgress } = useScroll();
 
   const [visible, setVisible] = useState(false);
@@ -35,7 +34,7 @@ export const FloatingNav = ({
   });
 
   return (
-    (<AnimatePresence mode="wait">
+    <AnimatePresence mode="wait">
       <motion.div
         initial={{
           opacity: 1,
@@ -49,27 +48,45 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 px-4 md:px-8 py-2  items-center justify-center space-x-4",
+          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 px-4 py-2 items-center justify-center space-x-4",
           className
-        )}>
-        {navItems.map((navItem, idx) => (
+        )}
+      >
+        {/* {navItems.map((navItem, idx) => (
           <Link
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
-            )}>
+              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-500  hover:text-neutral-700"
+            )}
+          >
             <span className="block sm:hidden">{navItem.icon}</span>
             <span className="hidden sm:block text-sm">{navItem.name}</span>
+            <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
           </Link>
-        ))}
-        {/* <button
-          className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
+        ))} */}
+        <Link href="/#services">
+          <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
+            <span className="block sm:hidden">Home</span>
+            <span className="hidden sm:block text-sm"><IconHome className="h-4 w-4 text-neutral-500 dark:text-white" /></span>
+            <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
+          </button>
+        </Link>
+        <Link href="/" >
+          <Image src="/osslogo.png" alt="logo" width={40} height={40} />
+        </Link>
+        <Link href="/contact">
+          <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
+            <span className="block sm:hidden">Contact</span>
+            <span className="hidden sm:block text-sm"><IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" /></span>
+            <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
+          </button>
+        </Link>
+        {/* <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
           <span>Login</span>
-          <span
-            className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
+          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
         </button> */}
       </motion.div>
-    </AnimatePresence>)
+    </AnimatePresence>
   );
 };
