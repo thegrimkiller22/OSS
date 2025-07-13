10f0e1eab3278d9f0cf88daafc715020831fde7c
thegrimkiller22 <anubhav44r@gmail.com>
2025-07-13 21:56:34 +0530

    try

--- src/app/components/layout/header/Logo/index.tsx
+++ src/app/components/layout/header/Logo/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 interface HeaderProps { }
+//try to keep the interface empty if no props are needed
 const Logo: React.FC<HeaderProps> = () => {
     return (
         <Link href="/">
